5276923984a62e6286754451d73659f26ccf6667
Bayero-abdul <bayeroa65@gmail.com>
2023-10-18 19:52:02 +0000

    Fix by applying .get() to Redis object

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,34 +16,34 @@ class Cache:
 
     def __init__(self):
         """Initializes a cache instance. """
-
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """takes a data argument and stores it in redis using
         a random key."""
-
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str,
             fn: Callable = None) -> Union[str, bytes, int, float, None]:
-        """converts the data back to the desired format. """
-
+        """retrieves and converts the data back to the desired format. """
         if key is None:
             return None
 
-        data = self._redis(key)
+        data = self._redis.get(key)
         return fn(data) if fn else data if data else None
 
     def get_str(self, key: str) -> str:
-        """converts the data to string"""
-        return str(self._redis.get(key))
+        """retrieves and converts the data to string. """
+        try:
+            return str(self._redis.get(key))
+        except Exception:
+            return None
 
     def get_int(self, key: str) -> int:
-        """converts the data to int"""
+        """retrieves and converts the data to int"""
         try:
             return int(self._redis.get(key))
         except Exception:

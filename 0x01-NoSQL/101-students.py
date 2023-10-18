d00c904c8f2b49549a506bbcb7c3fb0a30868a5f
Bayero-abdul <bayeroa65@gmail.ccom>
2023-10-17 18:10:34 -0700

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""101-students.py """
+
+
+def top_students(mongo_collection):
+    """Returns all students sorted by average score.
+
+    """
+
+    pipeline = [
+            {"$match": {}},
+            {"$group": {"_id": "$_id", "averageScore": {"$sum": "$topics.score" }}}
+    ]
+
+    return  mongo_collection.aggregate(pipeline)
+
